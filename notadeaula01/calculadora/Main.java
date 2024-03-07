6d128356b3b567fbaa02dcfd438c645483bbd884
Ana <anaclaraqb@gmail.com>
2024-03-07 18:26:42 -0300

    calculadora

--- notadeaula01/calculadora/Main.java
+++ notadeaula01/calculadora/Main.java
@@ -10,35 +10,27 @@ public class Main {
         do {
             System.out.println("Para soma, digite 1; Para subtração, digite 2; Para multiplicação, digite 3; Para divisão, digite 4.");
             int x = teclado.nextInt();
+
+            System.out.println("Primeiro número: ");
+            n1 = teclado.nextInt();
+            System.out.println("Segundo número: ");
+            n2 = teclado.nextInt();
+
             switch (x) {
                 case 1:
-                    System.out.println("Primeiro número: ");
-                    n1 = teclado.nextInt();
-                    System.out.println("Segundo número: ");
-                    n2 = teclado.nextInt();
-                    System.out.print(c.somar(n1, n2));
+                    System.out.print("Resultado da soma: " + c.somar(n1, n2));
                     break;
                 case 2:
-                    System.out.println("Primeiro número: ");
-                    n1 = teclado.nextInt();
-                    System.out.println("Segundo número: ");
-                    n2 = teclado.nextInt();
-                    System.out.print(c.subtrair(n1, n2));
+                    System.out.print("Resultado da subtração: " + c.subtrair(n1, n2));
                     break;
                 case 3:
-                    System.out.println("Primeiro número: ");
-                    n1 = teclado.nextInt();
-                    System.out.println("Segundo número: ");
-                    n2 = teclado.nextInt();
-                    System.out.print(c.multiplicar(n1, n2));
+                    System.out.print("Resultado da multiplicação: " + c.multiplicar(n1, n2));
                     break;
                 case 4:
-                    System.out.println("Primeiro número: ");
-                    n1 = teclado.nextInt();
-                    System.out.println("Segundo número: ");
-                    n2 = teclado.nextInt();
-                    System.out.printf("%.2f", c.dividir(n1, n2));
+                    System.out.printf("Resultado da divisão: %.2f", c.dividir(n1, n2));
                     break;
+                default:
+                    System.out.println("Opção inválida.");
             }
             System.out.println("\nPara realizar outra operação, digite 0.");
             repetir = teclado.nextInt();
